d54b0775f615123f8deb006baecd8c1c8444e40e
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-02-26 12:25:17 -0500

    added header layout

--- header.php
+++ header.php
@@ -17,7 +17,18 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<header class="site-header">
-        
+  <div class="site-header__sides">
+    <a href="#" class="logo">BLOG</a>
+  </div>
+  <div class="site-header__sides"> <a href="#" class="menu">-</a></div>
+  <div class="info">
+  <h4><a href="#category">UI DESIGN</a></h4>
+    <h1>Edgar's Career Portfolio</h1>
+    <div class="meta">
+      <a  href="https://twitter.com/nodws" target="_b" class="author"></a><br>
+      By <a href="https://twitter.com/nodws" target="_b">Edgar Gasca Rodriguez</a> on Feb 26, 2019
+    </div>
+  </div>
     </header>
 
 	<div id="content" class="site-content">
