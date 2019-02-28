de6ceb2d7db6b4674ffef0841b6da9f6f36c7368
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-02-28 10:10:59 -0500

    added site header title in wordpress

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
   <div class="site-header__sides"> <a href="#" class="site-header__menu">-</a></div>
   <div class="site-header__info">
   <h4><a href="https://github.com/edgararodriguez">Github</a></h4>
-    <h1 class="site-header__title">Edgar's Career Portfolio</h1>
+    <h1 class="site-header__title"><?php the_field('site-header__title');?></h1>
     <div class="site-header__meta">
       <a  href="https://www.instagram.com/egriii/" target="_b" class="site-header__author"></a><br>
       By <a href="https://www.instagram.com/egriii/" target="_b">Edgar Gasca Rodriguez</a>
--- template-home.php
+++ template-home.php
@@ -9,6 +9,7 @@ get_header();
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
   <p align="center"><a href="https://www.instagram.com/egriii/" class="btn twtr" target="_b">Follow me on Instagram</a>
   </p>
+
 </section>
 
 
