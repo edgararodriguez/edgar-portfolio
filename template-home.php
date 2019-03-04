9cf002164ee73c1ab1c8f5cef16be65d8c8cfa56
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-04 12:09:01 -0500

    added website youtube video along with my resume

--- template-home.php
+++ template-home.php
@@ -6,8 +6,11 @@
 get_header();
 ?>
 <section class="content">
+  <div>
+    <?php the_field('youtube_video');?>
+</div>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
-  <p align="center"><a href="https://www.instagram.com/egriii/" class="btn twtr" target="_b">Follow me on Instagram</a>
+  <p align="center"><a href="<?php the_field('site_resume');?>" class="btn twtr" target="_b">Check Out My Resume! ✏️🖥️</a>
   </p>
 
 </section>
