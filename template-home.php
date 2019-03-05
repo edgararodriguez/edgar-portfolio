0c02f6eae20119e8fa8c365248aff29048610835
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-05 10:02:02 -0500

    made the youtube video responsive

--- template-home.php
+++ template-home.php
@@ -9,9 +9,11 @@ get_header();
   <div class="content__youtube-video">
     <?php the_field('youtube_video');?>
 </div>
+<p align="center"><a href="<?php the_field('site_resume');?>" class="btn" target="_b">Check Out My Resume!✏️🖥️</a>
+</p>
 <p><?php the_field('paragraph_one_content');?></p>
 <p><?php the_field('paragraph_two_content');?></p>
-  <p align="center"><a href="<?php the_field('site_resume');?>" class="btn" target="_b">Check Out My Resume! ✏️🖥️</a>
+  <p align="center"><a href="<?php the_field('site_resume');?>" class="btn" target="_b">Check Out My Code!👨‍💻💻📚</a>
   </p>
 
 </section>
