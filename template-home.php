2ffeb1bc598d4ed3ab57be58cd7c50b1de187bb5
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-05 09:00:53 -0500

    added youtube video and centered it

--- template-home.php
+++ template-home.php
@@ -6,10 +6,11 @@
 get_header();
 ?>
 <section class="content">
-  <div>
+  <div class="content__youtube-video">
     <?php the_field('youtube_video');?>
 </div>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
+<p><?php the_field('paragraph_one_content');?></p>
+<p><?php the_field('paragraph_two_content');?></p>
   <p align="center"><a href="<?php the_field('site_resume');?>" class="btn twtr" target="_b">Check Out My Resume! ✏️🖥️</a>
   </p>
 
