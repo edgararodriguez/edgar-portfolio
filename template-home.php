d1a72242561b4b858391f55276da2cef55210aaf
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-06 10:29:49 -0500

    changed button class to bem

--- template-home.php
+++ template-home.php
@@ -9,7 +9,7 @@ get_header();
   <div class="content__youtube-video">
     <?php the_field('youtube_video');?>
 </div>
-<p align="center"><a href="<?php the_field('site_resume');?>" class="btn" target="_b">Check Out My Resume!✏️🖥️</a>
+<p align="center"><a href="<?php the_field('site_resume');?>" class="content__btn" target="_b">Check Out My Resume!✏️🖥️</a>
 </p>
 <p><?php the_field('paragraph_one_content');?></p>
 <p><?php the_field('paragraph_two_content');?></p>
