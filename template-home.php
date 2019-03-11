16ab3d25dca6f38311121655e66ce45140bd18d3
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-11 09:24:04 -0400

    added editor removed paragraphs

--- template-home.php
+++ template-home.php
@@ -10,11 +10,9 @@ get_header();
   <div class="content__youtube-video">
     <?php the_field('youtube_video');?>
 </div>
+<?php the_content(); ?>
 <p align="center"><a href="<?php the_field('site_resume');?>" class="content__btn" target="_b">Check Out My Resume!✏️🖥️</a>
 </p>
-<p><?php the_field('paragraph_one_content');?></p>
-<p><?php the_field('paragraph_two_content');?></p>
-
 </section>
 
 
