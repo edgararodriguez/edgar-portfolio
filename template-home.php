44aab8c45a5ea21c42c02425a09c5e00bb946347
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2019-03-11 11:30:10 -0400

    added site cover letter

--- template-home.php
+++ template-home.php
@@ -9,9 +9,11 @@ get_header();
   <p class="content__title">Welcome!</p>
   <div class="content__youtube-video">
     <?php the_field('youtube_video');?>
+    <p align="center"><a href="<?php the_field('site_resume');?>" class="content__btn" target="_b">Check Out My Resume!✏️🖥️</a>
+    </p>
 </div>
 <?php the_content(); ?>
-<p align="center"><a href="<?php the_field('site_resume');?>" class="content__btn" target="_b">Check Out My Resume!✏️🖥️</a>
+<p align="center"><a href="<?php the_field('site_cover-letter');?>" class="content__btn" target="_b">Check Out My Cover Letter!✉️📖</a>
 </p>
 </section>
 
